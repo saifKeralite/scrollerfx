d1e042362357494ed52efec3c7f3e672e51801fb
saif <nisaifudeen@gmail.com>
2023-11-14 22:30:10 +0530

    test project initial commit

---
+++ test/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "."),
+    },
+    compress: true,
+    port: 8080,
+  },
+};
